caee86adec67d9214f19318fde5c5eecd67c5820
Katherine Broner <katherine.broner@gmail.com>
2015-12-05 20:14:34 -0800

    completed questions 1 thru 3

---
+++ dictionaries.py
@@ -0,0 +1,5 @@
+# my_stats = {"name":"katherine","age":"23","height":"5'8"}
+# #print "%s is %s years old with a height of %s." % (my_stats["name"],my_stats["age"],my_stats["height"]) 
+# del my_stats["age"]
+# print my_stats
+
